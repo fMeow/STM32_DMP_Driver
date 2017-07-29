0c83ef7cc2d16ee5e2aa0321fb42fb570c5dee9d
Guoli Lv <guoli-lv@hotmail.com>
2017-07-29 16:48:29 +0800

    Fix I2C.h redefined issue.

--- MPU6050/I2C.h
+++ MPU6050/I2C.h
@@ -1,6 +1,8 @@
 #include <stm32f1xx_hal_def.h>
 #include "stdint.h"
 
+#ifndef __I2C_H_
+#define __I2C_H_
 HAL_StatusTypeDef i2c_write(uint8_t slave_addr, uint8_t reg_addr,
     uint8_t length, uint8_t const *data);
 HAL_StatusTypeDef i2c_read(uint8_t slave_addr, uint8_t reg_addr, uint8_t length,
@@ -9,3 +11,4 @@ HAL_StatusTypeDef IICwriteBit(uint8_t slave_addr, uint8_t reg_addr,
     uint8_t bitNum, uint8_t data);
 HAL_StatusTypeDef IICwriteBits(uint8_t slave_addr, uint8_t reg_addr,
     uint8_t bitStart, uint8_t length, uint8_t data);
+#endif
